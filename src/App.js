0d6ef06a567127b58aef25d7d2c02bc5a867d4b3
senyaq <senyaq@gmail.com>
2020-05-30 14:47:41 +0300

    Fix redirect to home

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
 import React, { useEffect, useState } from 'react'
-import { Redirect } from 'react-router-dom'
+import { useHistory } from 'react-router-dom'
 import Router from './Router'
 
 import './styles/global.scss'
-
+import { usersDB } from './lib/firebase'
 import app from 'firebase/app'
 
 const App = () => {
   const [isLoad, setIsload] = useState(false)
-  const [redirect, setRedirect] = useState(false)
-
+  const history = useHistory()
   useEffect(() => {
     app.auth().onAuthStateChanged(user => {
       let redirect = ''
       if (user) {
-        localStorage.setItem('auth', 'in')
-        redirect = '/main'
+        usersDB
+          .doc(user.uid)
+          .get()
+          .then(user => console.log('user', user.data()))
+        localStorage.setItem('user', 'in')
+        redirect = history.location.pathname
       } else {
         localStorage.setItem('auth', 'out')
         redirect = '/sign_in'
       }
       setIsload(true)
-      setRedirect(redirect)
+      history.push(redirect)
     })
-  }, [])
+  }, [history])
 
-  return (
-    <>
-      {redirect && <Redirect to={redirect} />}
-      {isLoad ? <Router /> : <h1>Loading....</h1>}
-    </>
-  )
+  return <>{isLoad ? <Router /> : <h1>Loading....</h1>}</>
 }
 
 export default App
