299e2f38cbf82c69f9e5b958c7f8c0f60f5c3725
senyaq <senyaq@gmail.com>
2020-06-06 14:45:33 +0300

    Add Backdrop

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import { useHistory } from 'react-router-dom'
 import Router from './Router'
 
 import './styles/global.scss'
+import Backdrop from '@material-ui/core/Backdrop'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import { makeStyles } from '@material-ui/core/styles'
+
 import { usersCLL } from './lib/firebase'
 import app from 'firebase/app'
 import { UserProvider } from './context/UserContext'
+
+const useStyles = makeStyles((theme) => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: '#fff',
+  },
+}))
 
 const App = () => {
   const [isLoad, setIsload] = useState(false)
   const history = useHistory()
   const [user, setUser] = useState(null)
-
+  const classes = useStyles()
   useEffect(() => {
     app.auth().onAuthStateChanged((user) => {
       let redirect = ''
@@ -34,7 +45,17 @@ const App = () => {
     })
   }, [history])
 
-  return <UserProvider value={{ user }}>{isLoad ? <Router /> : <h1>Loading....</h1>}</UserProvider>
+  return (
+    <UserProvider value={{ user }}>
+      {isLoad ? (
+        <Router />
+      ) : (
+        <Backdrop className={classes.backdrop} open={true}>
+          <CircularProgress color="inherit" />
+        </Backdrop>
+      )}
+    </UserProvider>
+  )
 }
 
 export default App
